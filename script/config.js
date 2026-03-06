fdaabad8ae12be46184f62fa920d9adc3aeacd45
tfshorifulislam <tfshorifulislam@gmail.com>
2026-03-07 05:02:23 +0600

    closed card function add

--- script/config.js
+++ script/config.js
@@ -113,12 +113,12 @@ switchBtnParent.addEventListener('click', (event) => {
         openIssue()
 
         const openIssueDisplay = (openIssue) => {
-          contentDisplay.innerHTML = '';
+            contentDisplay.innerHTML = '';
 
-          openIssue.forEach((item, number) => {
-             const newDiv = document.createElement('div');
-             const serial = number + 1
-             newDiv.innerHTML = `
+            openIssue.forEach((item, number) => {
+                const newDiv = document.createElement('div');
+                const serial = number + 1
+                newDiv.innerHTML = `
              
              
                  <div class="">
@@ -161,8 +161,8 @@ switchBtnParent.addEventListener('click', (event) => {
              
              
              `
-             contentDisplay.appendChild(newDiv);
-          });
+                contentDisplay.appendChild(newDiv);
+            });
 
         }
     }
@@ -179,6 +179,66 @@ switchBtnParent.addEventListener('click', (event) => {
         openBtn.classList.add(...inActiveBtn)
 
 
+        // api fetch;
+        const closeIssue = () => {
+            const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`
+            fetch(url)
+                .then(res => res.json())
+                .then(data => closeIssueDisplay(data.data.filter(issue => issue.status === 'closed')))
+        }
+
+        // close issue display;
+        const closeIssueDisplay = (closeIssue, number)=>{
+            contentDisplay.innerHTML = ''
+
+            const serial = number + 1;
+
+            closeIssue.forEach(item => {
+                const newDiv = document.createElement('div')
+                newDiv.innerHTML =`
+                
+                 <div class="">
+                    <div class="card-top-part bg-white p-4 shadow-sm">
+                        <div class="flex justify-between mb-3">
+                            <img class="w-6 h-6" src="./assets/Open-Status.png" alt="">
+                            <p class="font-medium text-sm py-1 px-6 rounded-full 
+                            ${item.priority === 'medium' ? 'bg-[#FFF6D1] text-[#F59E0B]' :
+                        item.priority === 'low' ? 'bg-[#EEEFF2] text-[#9CA3AF]' :
+                            'text-[#EF4444] bg-[#FEECEC]'
+                    }
+                            ">${item.priority}</p>
+                        </div>
+                        <h1 class="font-semibold text-[#1F2937] text-sm mb-2">${item.title}</h1>
+                        <p class="text-[#64748B] line-clamp-2 mb-3">${item.description}</p>
+
+                        <div class="flex flex-col gap-2 md:flex-row md:justify-between">
+                            <div
+                                class=" flex items-center justify-center gap-2 text-[#EF4444] uppercase font-bold bg-[#FEECEC] border border-[#FECACA] rounded-full px-7 py-2">
+                                <img class="w-3 h-3" src="./assets/bug.png" alt="">
+                                <p>${item.status}</p> 
+                            </div>
+
+                            <div
+                                class=" flex items-center justify-center gap-2 text-[#D97706] uppercase font-bold bg-[#FFF8DB] border border-[#FDE68A] rounded-full px-7 py-2">
+                                <img class="w-3 h-3" src="./assets/bug.png" alt="">
+                                <p>${item.status}</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="card-bottom-part bg-white shadow-sm p-4 space-y-2 border border-[#E4E4E7] text-[#64748B] text-sm ">
+                        <p>#${serial} ${item.author}</p>
+                        <p>${item.createdAt}</p>
+                    </div>
+                </div>
+                
+                
+                `
+                contentDisplay.appendChild(newDiv)
+            });
+        }
+        closeIssue()
     }
 })
 
