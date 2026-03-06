235e915c07b51ef48eb62ea7cd08f6c67a0ab5ba
tfshorifulislam <tfshorifulislam@gmail.com>
2026-03-07 04:48:34 +0600

    open issue API add

--- script/config.js
+++ script/config.js
@@ -53,9 +53,9 @@ switchBtnParent.addEventListener('click', (event) => {
                             <img class="w-6 h-6" src="./assets/Open-Status.png" alt="">
                             <p class="font-medium text-sm py-1 px-6 rounded-full 
                             ${item.priority === 'medium' ? 'bg-[#FFF6D1] text-[#F59E0B]' :
-                                item.priority === 'low' ? 'bg-[#EEEFF2] text-[#9CA3AF]' :
-                                'text-[#EF4444] bg-[#FEECEC]'
-                            }
+                        item.priority === 'low' ? 'bg-[#EEEFF2] text-[#9CA3AF]' :
+                            'text-[#EF4444] bg-[#FEECEC]'
+                    }
                             ">${item.priority}</p>
                         </div>
                         <h1 class="font-semibold text-[#1F2937] text-sm mb-2">${item.title}</h1>
@@ -65,7 +65,7 @@ switchBtnParent.addEventListener('click', (event) => {
                             <div
                                 class=" flex items-center justify-center gap-2 text-[#EF4444] uppercase font-bold bg-[#FEECEC] border border-[#FECACA] rounded-full px-7 py-2">
                                 <img class="w-3 h-3" src="./assets/bug.png" alt="">
-                                <p>${item.status}</p>
+                                <p>${item.status}</p> 
                             </div>
 
                             <div
@@ -97,13 +97,76 @@ switchBtnParent.addEventListener('click', (event) => {
         openBtn.classList.add(...activeBtn)
 
         allBtn.classList.remove(...activeBtn)
-        allBtn.classList.remove(...activeBtn)
+        closeBtn.classList.remove(...activeBtn)
 
+        allBtn.classList.add(...inActiveBtn)
         closeBtn.classList.add(...inActiveBtn)
-        closeBtn.classList.add(...inActiveBtn)
 
+        // open api fetch;
+        const openIssue = (id) => {
+            const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`
+            // console.log(url)
+            fetch(url)
+                .then(res => res.json())
+                .then(data => openIssueDisplay(data.data.filter(issue => issue.status === 'open')))
+        }
+        openIssue()
 
+        const openIssueDisplay = (openIssue) => {
+          contentDisplay.innerHTML = '';
+
+          openIssue.forEach((item, number) => {
+             const newDiv = document.createElement('div');
+             const serial = number + 1
+             newDiv.innerHTML = `
+             
+             
+                 <div class="">
+                    <div class="card-top-part bg-white p-4 shadow-sm">
+                        <div class="flex justify-between mb-3">
+                            <img class="w-6 h-6" src="./assets/Open-Status.png" alt="">
+                            <p class="font-medium text-sm py-1 px-6 rounded-full 
+                            ${item.priority === 'medium' ? 'bg-[#FFF6D1] text-[#F59E0B]' :
+                        item.priority === 'low' ? 'bg-[#EEEFF2] text-[#9CA3AF]' :
+                            'text-[#EF4444] bg-[#FEECEC]'
+                    }
+                            ">${item.priority}</p>
+                        </div>
+                        <h1 class="font-semibold text-[#1F2937] text-sm mb-2">${item.title}</h1>
+                        <p class="text-[#64748B] line-clamp-2 mb-3">${item.description}</p>
+
+                        <div class="flex flex-col gap-2 md:flex-row md:justify-between">
+                            <div
+                                class=" flex items-center justify-center gap-2 text-[#EF4444] uppercase font-bold bg-[#FEECEC] border border-[#FECACA] rounded-full px-7 py-2">
+                                <img class="w-3 h-3" src="./assets/bug.png" alt="">
+                                <p>${item.status}</p> 
+                            </div>
+
+                            <div
+                                class=" flex items-center justify-center gap-2 text-[#D97706] uppercase font-bold bg-[#FFF8DB] border border-[#FDE68A] rounded-full px-7 py-2">
+                                <img class="w-3 h-3" src="./assets/bug.png" alt="">
+                                <p>${item.status}</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="card-bottom-part bg-white shadow-sm p-4 space-y-2 border border-[#E4E4E7] text-[#64748B] text-sm ">
+                        <p>#${serial} ${item.author}</p>
+                        <p>${item.createdAt}</p>
+                    </div>
+                </div>
+
+             
+             
+             
+             `
+             contentDisplay.appendChild(newDiv);
+          });
+
+        }
     }
+
 
     else if (event.target.id === 'close-btn') {
         closeBtn.classList.remove(...inActiveBtn)
